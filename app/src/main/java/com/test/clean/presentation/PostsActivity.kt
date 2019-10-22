c953d3a25213bc4c54fc7850422d9bc6ad0e88a1
jvillad1 <jvillad1@gmail.com>
2019-10-22 07:43:50 -0500

    Adding Empty Activity & Custom Application

---
+++ app/src/main/java/com/test/clean/presentation/PostsActivity.kt
@@ -0,0 +1,13 @@
+package com.test.clean.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.clean.R
+
+class PostsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts)
+    }
+}
